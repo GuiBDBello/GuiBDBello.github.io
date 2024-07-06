74085502c6ce1098e6aabe63a55766c99758ba81
GuiBDBello <guilhermedallbello@hotmail.com>
2024-07-05 22:43:00 -0300

    Update games

--- js/itchIoGames.js
+++ js/itchIoGames.js
@@ -1,396 +1,98 @@
 let myGames = {
     "games": [
         {
-            "p_windows": true,
-            "p_osx": true,
-            "p_linux": true,
-            "id": 2080344,
-            "short_text": "Discover the Ancient Secrets.",
-            "title": "Ancient Secrets",
-            "published": true,
-            "created_at": "2023-05-21 19:19:43",
-            "in_press_system": false,
-            "published_at": "2023-05-21 19:38:03",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/ancient-secrets",
-            "downloads_count": 20,
-            "views_count": 66,
-            "p_android": false,
-            "can_be_bought": false,
-            "embed": {
-                "width": 1152,
-                "height": 648,
-                "fullscreen": false
-            },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU2OTg3LnBuZw==\/315x250%23c\/GvzuWz.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": false,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 2704326,
-            "short_text": "Escape the Void, save yourself.",
-            "title": "aVoid (the Void)",
-            "published": true,
-            "created_at": "2024-05-12 19:24:43",
-            "in_press_system": false,
-            "published_at": "2024-05-12 19:26:22",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "can_be_bought": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/avoid-the-void",
-            "downloads_count": 0,
-            "views_count": 67,
-            "p_android": false,
-            "embed": {
-                "width": 1152,
-                "height": 648,
-                "fullscreen": false
-            },
-            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNzk4LmdpZg==\/315x250%23cm\/FmpiEx.gif",
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNzk4LmdpZg==\/315x250%23c\/7G8Kux.gif",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 524354,
-            "short_text": "Parkour versus game.",
-            "title": "Block and Run",
-            "published": true,
-            "created_at": "2019-11-26 23:16:28",
-            "in_press_system": false,
-            "published_at": "2019-12-01 21:09:46",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "default",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/block-and-run",
-            "downloads_count": 24,
-            "views_count": 141,
-            "p_android": false,
-            "can_be_bought": false,
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzI3MzkyNTcucG5n\/315x250%23c\/qipaed.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 655822,
-            "short_text": "Conscientize people to save the Environment.",
-            "title": "Conscientize",
-            "published": true,
-            "created_at": "2020-05-28 02:33:26",
-            "in_press_system": false,
-            "published_at": "2020-05-28 02:35:11",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/conscientize",
-            "downloads_count": 12,
-            "views_count": 83,
-            "p_android": false,
-            "can_be_bought": false,
-            "embed": {
-                "width": 1152,
-                "height": 648,
-                "fullscreen": false
-            },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzM1MjM5NjMucG5n\/315x250%23c\/wzeVf3.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 757748,
-            "short_text": "Unexpected first-person platformer.",
-            "title": "Dog Sonic",
-            "published": true,
-            "created_at": "2020-09-14 06:05:20",
-            "in_press_system": false,
-            "published_at": "2020-09-14 06:10:58",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/dog-sonic",
-            "downloads_count": 45,
-            "views_count": 127,
-            "p_android": false,
-            "can_be_bought": false,
-            "embed": {
-                "width": 1152,
-                "height": 648,
-                "fullscreen": false
-            },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzQyMzU1MjMucG5n\/315x250%23c\/nkviGB.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": false,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 2692675,
-            "short_text": "3D maze generation powered by Godot.",
-            "title": "Godot Maze Generator",
-            "published": true,
-            "created_at": "2024-05-07 01:12:00",
-            "in_press_system": false,
-            "published_at": "2024-05-08 11:28:14",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "can_be_bought": false,
-            "type": "html",
-            "min_price": 0,
             "url": "https:\/\/guibdbello.itch.io\/godot-maze-generator",
-            "downloads_count": 0,
-            "views_count": 19,
+            "p_windows": false,
+            "p_osx": false,
+            "p_linux": false,
             "p_android": false,
-            "embed": {
-                "width": 1152,
-                "height": 648,
-                "fullscreen": false
-            },
-            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU3NDM0LmdpZg==\/315x250%23cm\/jc1wRs.gif",
+            "type": "html",
             "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU3NDM0LmdpZg==\/315x250%23c\/mNmScz.gif",
             "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 940743,
-            "short_text": "Sometimes you must fail before you succeed.",
-            "title": "Sacrifice",
-            "published": true,
-            "created_at": "2021-03-01 12:21:49",
+            "title": "Godot Maze Generator",
+            "published_at": "2024-05-08 11:28:14",
             "in_press_system": false,
-            "published_at": "2021-03-01 13:56:43",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
+            "created_at": "2024-05-07 01:12:00",
+            "purchases_count": 0,
             "has_demo": false,
-            "type": "html",
+            "downloads_count": 0,
+            "user": {
+                "display_name": "GuiBDBello",
+                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
+                "url": "https:\/\/guibdbello.itch.io",
+                "id": 1304622,
+                "username": "GuiBDBello"
+            },
+            "views_count": 19,
+            "short_text": "3D maze generation powered by Godot.",
+            "id": 2692675,
             "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/sacrifice",
-            "downloads_count": 9,
-            "views_count": 81,
-            "p_android": false,
+            "published": true,
             "can_be_bought": false,
             "embed": {
-                "width": 1152,
                 "height": 648,
+                "width": 1152,
                 "fullscreen": false
             },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzUzNDU4ODEucG5n\/315x250%23c\/eRJB%2FW.png",
-            "classification": "game",
-            "purchases_count": 0
+            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU3NDM0LmdpZg==\/315x250%23cm\/jc1wRs.gif"
         },
         {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 338050,
-            "short_text": "Control a sphere to avoid geometric 'evil' objects",
-            "title": "Shapezoid",
-            "published": true,
-            "created_at": "2018-12-01 19:28:27",
-            "in_press_system": false,
-            "published_at": "2018-12-05 16:56:06",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "default",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/shapezoid",
-            "downloads_count": 18,
-            "views_count": 121,
-            "p_android": false,
-            "can_be_bought": false,
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzY5MjQucG5n\/315x250%23c\/hHvPQ%2B.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
+            "url": "https:\/\/guibdbello.itch.io\/labiralg",
             "p_windows": false,
             "p_osx": false,
             "p_linux": false,
-            "id": 463054,
-            "short_text": "You have to complete each level in only ONE SHOT!",
-            "title": "The Shot!",
-            "published": true,
-            "created_at": "2019-08-04 18:46:11",
-            "in_press_system": false,
-            "published_at": "2019-08-04 18:54:44",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/the-shot",
-            "downloads_count": 1,
-            "views_count": 80,
             "p_android": false,
-            "can_be_bought": false,
-            "embed": {
-                "width": 800,
-                "height": 600,
-                "fullscreen": false
-            },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzUzMDYxNzMucG5n\/315x250%23c\/8Lnx0h.png",
+            "type": "html",
+            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2ODE4MzQ1LmdpZg==\/315x250%23c\/RZli4Q.gif",
             "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": false,
-            "id": 729689,
-            "short_text": "Treasure hunting multiplayer communication game.",
-            "title": "Treasure Chasers",
-            "published": true,
-            "created_at": "2020-08-13 23:22:38",
+            "title": "Labiralg",
+            "published_at": "2024-07-05 11:45:32",
             "in_press_system": false,
-            "published_at": "2020-08-13 23:41:36",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
+            "created_at": "2024-07-03 22:02:04",
+            "purchases_count": 0,
             "has_demo": false,
-            "type": "html",
+            "downloads_count": 0,
+            "user": {
+                "display_name": "GuiBDBello",
+                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
+                "url": "https:\/\/guibdbello.itch.io",
+                "id": 1304622,
+                "username": "GuiBDBello"
+            },
+            "views_count": 9,
+            "short_text": "Procedurally generated maze game.",
+            "id": 2811761,
             "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/treasure-chasers",
-            "downloads_count": 22,
-            "views_count": 159,
-            "p_android": false,
+            "published": true,
             "can_be_bought": false,
             "embed": {
-                "width": 1152,
                 "height": 648,
-                "fullscreen": false
-            },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzQwNTQ5NDQucG5n\/315x250%23c\/ZdkIXB.png",
-            "classification": "game",
-            "purchases_count": 0
-        },
-        {
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": true,
-            "id": 476861,
-            "short_text": "Do not trust \"The Game\"!",
-            "title": "Unreliable",
-            "published": true,
-            "created_at": "2019-08-31 16:32:23",
-            "in_press_system": false,
-            "published_at": "2019-08-31 17:06:49",
-            "user": {
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "display_name": "GuiBDBello",
-                "id": 1304622,
-                "username": "GuiBDBello",
-                "url": "https:\/\/guibdbello.itch.io"
-            },
-            "has_demo": false,
-            "type": "html",
-            "min_price": 0,
-            "url": "https:\/\/guibdbello.itch.io\/unreliable",
-            "downloads_count": 29,
-            "views_count": 94,
-            "p_android": false,
-            "can_be_bought": false,
-            "embed": {
                 "width": 1152,
-                "height": 648,
                 "fullscreen": false
             },
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzI0ODA3MDYucG5n\/315x250%23c\/2p9uRs.png",
-            "classification": "game",
-            "purchases_count": 0
+            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2ODE4MzQ1LmdpZg==\/315x250%23cm\/EkOMw3.gif"
         }
     ]
-}
+};
 
 let i = 0;
 myGames.games.sort((theyest, thou) => {
-    if (theyest.created_at > thou.created_at)
+    // Labiralg deve ser o último da lista
+    if (theyest.title == 'Labiralg')
+        return 1;
+    if (thou.title == 'Labiralg')
         return -1;
-    else if (theyest.created_at < thou.created_at)
+    
+    // Ordena pela data de publicação
+    if (theyest.published_at > thou.published_at)
+        return -1;
+    else if (theyest.published_at < thou.published_at)
         return 1;
     else
         return 0;
 }).forEach(game => {
     // Create Elements
-    let section = document.getElementById('game-jam-section');
+    let section = document.getElementById('games-section');
 
     let divGame = document.createElement('div');
 
@@ -432,6 +134,10 @@ myGames.games.sort((theyest, thou) => {
     divDescription.appendChild(aName);
     divDescription.appendChild(pDescription);
     divImage.appendChild(aImg);
+
+    console.log('divGame', divGame);
+    console.log('divDescription', divDescription);
+    console.log('divImage', divImage);
 
     divGame.appendChild(divDescription);
     divGame.appendChild(divImage);
--- js/itchIoGamesGameJam.js
+++ js/itchIoGamesGameJam.js
@@ -1,32 +1,5 @@
-{
+let myGamesGameJam = {
     "games": [
-        {
-            "url": "https:\/\/guibdbello.itch.io\/alura-apocalipse-zumbi",
-            "p_windows": true,
-            "p_osx": false,
-            "p_linux": true,
-            "p_android": false,
-            "type": "default",
-            "classification": "game",
-            "published": true,
-            "views_count": 3,
-            "downloads_count": 3,
-            "purchases_count": 0,
-            "in_press_system": false,
-            "has_demo": false,
-            "id": 396366,
-            "min_price": 0,
-            "title": "Alura - Apocalipse Zumbi",
-            "can_be_bought": false,
-            "created_at": "2019-03-26 01:50:47",
-            "user": {
-                "display_name": "GuiBDBello",
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "url": "https:\/\/guibdbello.itch.io",
-                "id": 1304622,
-                "username": "GuiBDBello"
-            }
-        },
         {
             "url": "https:\/\/guibdbello.itch.io\/ancient-secrets",
             "p_windows": true,
@@ -199,78 +172,6 @@
             "in_press_system": false
         },
         {
-            "url": "https:\/\/guibdbello.itch.io\/godot-maze-generator",
-            "p_windows": false,
-            "p_osx": false,
-            "p_linux": false,
-            "p_android": false,
-            "type": "html",
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU3NDM0LmdpZg==\/315x250%23c\/mNmScz.gif",
-            "classification": "game",
-            "title": "Godot Maze Generator",
-            "published_at": "2024-05-08 11:28:14",
-            "in_press_system": false,
-            "created_at": "2024-05-07 01:12:00",
-            "purchases_count": 0,
-            "has_demo": false,
-            "downloads_count": 0,
-            "user": {
-                "display_name": "GuiBDBello",
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "url": "https:\/\/guibdbello.itch.io",
-                "id": 1304622,
-                "username": "GuiBDBello"
-            },
-            "views_count": 19,
-            "short_text": "3D maze generation powered by Godot.",
-            "id": 2692675,
-            "min_price": 0,
-            "published": true,
-            "can_be_bought": false,
-            "embed": {
-                "height": 648,
-                "width": 1152,
-                "fullscreen": false
-            },
-            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzU3NDM0LmdpZg==\/315x250%23cm\/jc1wRs.gif"
-        },
-        {
-            "url": "https:\/\/guibdbello.itch.io\/labiralg",
-            "p_windows": false,
-            "p_osx": false,
-            "p_linux": false,
-            "p_android": false,
-            "type": "html",
-            "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2ODE4MzQ1LmdpZg==\/315x250%23c\/RZli4Q.gif",
-            "classification": "game",
-            "title": "Labiralg",
-            "published_at": "2024-07-05 11:45:32",
-            "in_press_system": false,
-            "created_at": "2024-07-03 22:02:04",
-            "purchases_count": 0,
-            "has_demo": false,
-            "downloads_count": 0,
-            "user": {
-                "display_name": "GuiBDBello",
-                "cover_url": "https:\/\/img.itch.zone\/aW1nLzE2NzcxNjY2LmpwZw==\/100x100%23\/QKjKfy.jpg",
-                "url": "https:\/\/guibdbello.itch.io",
-                "id": 1304622,
-                "username": "GuiBDBello"
-            },
-            "views_count": 9,
-            "short_text": "Procedurally generated maze game.",
-            "id": 2811761,
-            "min_price": 0,
-            "published": true,
-            "can_be_bought": false,
-            "embed": {
-                "height": 648,
-                "width": 1152,
-                "fullscreen": false
-            },
-            "still_cover_url": "https:\/\/img.itch.zone\/aW1nLzE2ODE4MzQ1LmdpZg==\/315x250%23cm\/EkOMw3.gif"
-        },
-        {
             "url": "https:\/\/guibdbello.itch.io\/sacrifice",
             "p_windows": true,
             "p_osx": false,
@@ -441,4 +342,65 @@
             "in_press_system": false
         }
     ]
-}
+};
+
+let j = 0;
+myGamesGameJam.games.sort((theyest, thou) => {
+    if (theyest.published_at > thou.published_at)
+        return -1;
+    else if (theyest.published_at < thou.published_at)
+        return 1;
+    else
+        return 0;
+}).forEach(game => {
+    // Create Elements
+    let section = document.getElementById('game-jam-section');
+
+    let divGame = document.createElement('div');
+
+    let divDescription = document.createElement('div');
+    let aName = document.createElement('a');
+    let pDescription = document.createElement('p');
+
+    let divImage = document.createElement('div');
+    let aImg = document.createElement('a');
+    let img = document.createElement('img');
+
+    // Add Classes
+    divGame.classList.add('game');
+    divDescription.classList.add('game-description');
+    divImage.classList.add('game-image');
+
+    // Add Animation
+    divGame.setAttribute("data-aos", "zoom-in-up");
+    divGame.setAttribute("data-aos-anchor-placement", "center-bottom");
+
+    // Add Content
+    aName.href = game.url;
+    aName.target = '_blank';
+    aName.textContent = game.title;
+    pDescription.textContent = game.short_text;
+
+    aImg.href = game.url;
+    aImg.target = '_blank';
+    img.src = game.cover_url;
+
+    // Add Tilt
+    img.setAttributeNode(document.createAttribute("data-tilt"));
+    img.setAttribute("data-tilt-reverse", "true");
+    img.setAttribute("data-tilt-scale", "1.1");
+
+    // Append Childs
+    aImg.appendChild(img);
+
+    divDescription.appendChild(aName);
+    divDescription.appendChild(pDescription);
+    divImage.appendChild(aImg);
+
+    divGame.appendChild(divDescription);
+    divGame.appendChild(divImage);
+
+    section.appendChild(divGame);
+
+    j++;
+});
